ba36a98f9a03eaa43a7a9b567cbaf78835db5f8c
Anish S <anish180398@gmail.com>
2021-02-18 13:44:02 +0530

    Initial Commit

---
+++ config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+    mailer: {
+        service: 'Gmail',
+        auth:{
+            user: 'anish18398@gmail.com',
+            pass:'RebelUnited'
+        }
+       
+    },
+    dbConnstring: 'mongodb://localhost:27017/colabeditor',
+    sessionKey: 'Colabeditor'
+}
